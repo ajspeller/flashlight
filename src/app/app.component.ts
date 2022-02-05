f749b054d15b5a1f4fcc17cc4d10b596a9442f13
AJ Speller <ajspeller@gmail.com>
2022-02-05 16:11:04 -0500

    Add SplashScreen and StatusBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      await SplashScreen.hide();
+    } catch (error) {
+      console.warn({ error });
+    }
+
+    try {
+      await StatusBar.setBackgroundColor({ color: '#000' });
+    } catch (error) {
+      console.warn({ error });
+    }
+  }
 }
